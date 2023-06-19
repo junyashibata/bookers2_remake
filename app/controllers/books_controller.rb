8b6864681a47ff7e7c4729b8511372ca3c57ed7b
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-19 13:47:59 +0000

    5 kadai-3 before validation flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,18 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to books_path
   end
-
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+  
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get "homes/about" => 'homes#about', as:"about"
   
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :update, :new, :create, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
