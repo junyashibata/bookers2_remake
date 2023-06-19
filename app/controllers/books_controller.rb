58aecdca15b771daaa8731952cefbed16c639206
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-19 04:40:26 +0000

    5 kadai-3 before making user-index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,14 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book)
   end
-    
+
 
   def index
     @books = Book.all
@@ -17,8 +17,10 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book_new = Book.new
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def edit
