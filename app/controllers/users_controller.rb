5ab76aef43769faec457963867560466bc9f2f0e
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-20 04:52:23 +0000

    5 kadai-3 making flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,6 @@ class UsersController < ApplicationController
     @book = Book.new
   end
   
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
-  end
 
   def edit
     @user = User.find(params[:id])
