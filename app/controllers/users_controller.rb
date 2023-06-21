78201166ec7484916a8a8b4b223ff91c05e2d03a
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-21 04:50:55 +0000

    finish kadai-3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
-  
-  
+
+
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book_new = Book.new
   end
-  
+
   def new
     @book = Book.new
   end
-  
+
 
   def edit
     @user = User.find(params[:id])
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     redirect_to user_path(current_user)
     end
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -32,13 +32,13 @@ class UsersController < ApplicationController
     render :edit
   end
   end
-  
+
   def index
     @users = User.all
     @user = current_user
     @book_new = Book.new
   end
-  
+
     private
 
   def user_params
